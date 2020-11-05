825f97e0fae13c0bfedee467bf0ef009864e3e3b
Henrique Vilela <henrique@pixelti.com.br>
2020-11-05 11:34:17 -0300

    comment updated

--- src/controllers/crawler.controller.js
+++ src/controllers/crawler.controller.js
@@ -1,6 +1,7 @@
 const HttpAdapter = require('../util/http-adapter/http-adapter');
 const giic = require('../util/get-information/get-information-in-component');
 
+// Get HTML page for crawler and pass to giic.GetInformatio() to extract data
 exports.getTransferTax = async (url) => {
     let remoteData = await new HttpAdapter().get(url);
     const html = remoteData.body;
